6c574da3f15e0741d6b14433d10ab50174ee9cc7
ManasSin <maltisadan12@gmail.com>
2024-01-01 12:20:28 +0530

    FEAT: working on login modal

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import Loginform from "@/components/auth/loginForm";
+import { FC } from "react";
+
+interface pageProps {}
+
+const LoginPage: FC<pageProps> = ({}) => {
+  return <Loginform />;
+};
+
+export default LoginPage;

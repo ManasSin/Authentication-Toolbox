5cbe7029f4be4415e1c93b1a54f750250205d444
ManasSin <maltisadan12@gmail.com>
2024-01-02 12:13:07 +0530

    ADD: register schema added for register form.

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,13 @@ export const LoginSchema = z.object({
   }),
   password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Provide propper email address",
+  }),
+  password: z.string().min(1, { message: "Password is required" }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});

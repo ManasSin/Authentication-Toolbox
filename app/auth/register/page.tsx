f75b225215956ae55628d6472f4bf3d72c12f2eb
ManasSin <maltisadan12@gmail.com>
2024-01-02 12:10:25 +0530

    FEAT: added register route.

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import RegisterForm from "@/components/auth/RegisterPage";
+
+interface pageProps {}
+
+const RegisterPage: FC<pageProps> = ({}) => {
+  return <RegisterForm />;
+};
+
+export default RegisterPage;

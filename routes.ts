39f0a415558fc19ac06cb868b76cca6cbb3689d1
ManasSin <maltisadan12@gmail.com>
2024-01-03 15:39:25 +0530

    ADD:  routes for public and authenticated access

---
+++ routes.ts
@@ -0,0 +1,30 @@
+/**
+ * An array of routes that are accessible to the public
+ * These eoutes do not require authentication
+ * @type {sring[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type {sring[]}
+ */
+
+export const authRoutes = ["/auth/login", "auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purpose
+ * @type {sring[]}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after loggin in
+ * @type {sring}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";

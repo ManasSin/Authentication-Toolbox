deac6eb16bef398889435ca072c2dfdf8178be2d
ManasSin <maltisadan12@gmail.com>
2024-01-03 15:39:51 +0530

    ADD:  Radix icons to Icons component

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,4 +1,11 @@
+// react-icons
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
-export { FcGoogle, FaGithub };
+// radix icons
+import {
+  ExclamationTriangleIcon,
+  CheckCircledIcon,
+} from "@radix-ui/react-icons";
+
+export { FcGoogle, FaGithub, ExclamationTriangleIcon, CheckCircledIcon };

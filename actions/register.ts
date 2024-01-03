14062d1388c19ba2c739c79aacf6383a7c15dd96
ManasSin <maltisadan12@gmail.com>
2024-01-03 15:39:34 +0530

    Add register action for user registration

---
+++ actions/register.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
+import { TRegisterSchema } from "@/types/schemaTypes";
+import { getUserByEmail } from "@/lib/getUsers";
+// learn about progresive inhancement in server action, for register.
+
+export const register = async (values: TRegisterSchema) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success)
+    return {
+      error: "Invalid fields",
+    };
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) return { error: "Email already taken" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created! 🎉" };
+};

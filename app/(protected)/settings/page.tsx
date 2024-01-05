dbfac697c09e17ae1fc5fc530952234e9860ed9e
ManasSin <maltisadan12@gmail.com>
2024-01-05 23:50:23 +0530

    ADD: sign out functionality and create AuthErrorPage component

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,24 @@
 import { FC } from "react";
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 interface pageProps {}
 
 const page: FC<pageProps> = async ({}) => {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button variant="default">Sign out</Button>
+      </form>
+      {JSON.stringify(session)}
+    </div>
+  );
 };
 
 export default page;
---
+++ app/auth/error/page.tsx
@@ -0,0 +1,10 @@
+import ErrorCards from "@/components/auth/ErrorCards";
+import React from "react";
+
+type Props = {};
+
+const AuthErrorPage = (props: Props) => {
+  return <ErrorCards />;
+};
+
+export default AuthErrorPage;
